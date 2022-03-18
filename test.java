b6f87686a4d21150c3c1a709c29ee520b98789f9
Andrew <andreworoh.ao@gmail.com>
2022-03-18 13:58:13 +0800

    delete previous and create new java file

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a = 10;
+        int b = 20;
+        System.out.println("hasil = "+ (a+b));
+    }
+}
